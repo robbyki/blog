d87baa3f5df70f7a5a84842df709559f9aee83eb
Robby Kiskanyan <rkee@us.ibm.com>
2021-12-15 08:38:13 -0500

    updating intro

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -36,77 +36,77 @@ const IndexPage = () => {
             that bring joy to developers. I've spent most of my career helping
             modernize data intensive stacks using open source technologies. I
             started my career in engineering at Yahoo using a lot of Python,
-            Pandas, Spark and SQL in conjunction with AWS services for
-            analyzing and prototyping models prior to production deployment. I
-            then spent several years at IBM where I have built Spark
-            development environments with Scala and sbt for deployments to
-            Kubernetes. I've been developing in Fedora and Redhat environments
-            for a long time and switching to Linux, as painful as it can be at
-            times has probably been the single most important decision I have
-            made in my career. I enjoy creating videos showcasing my interests
-            with all things code, linux and CLI tooling.
+            Pandas, Spark and SQL helping to wrangle and visualize massive
+            amounts of data for the ad exchange. After Yahoo I joined the
+            Weather Channel to help launch the first of its kind Data Science
+            platform to enable weather triggered ads on AWS services with
+            Spark. More recently I have built Spark development environments
+            with Scala and sbt for deployments to Kubernetes at IBM. I've been
+            developing in Fedora and Redhat environments for a long time and
+            recently transitioned to a completely neovim based environment for
+            development.
             </span>
           </div>
         </div>
       </div>
     </Layout>
   )
-      // <div className={style.card}>
-      //   <div className={style.post}>
-      //     <div className={style.cover}>
-      //       <a
-      //         target="_blank"
-      //         rel="nofollow noopener noreferrer"
-      //         href={"https://github.com/robbyki/blog"}
-      //       >
-      //         <img src={ThisBlog} height="150px" width="225px" alt="blog" />
-      //       </a>
-      //     </div>
-      //     <div className={style.content}>
-      //       <span>Is available on my github you can fork it if you want.</span>
-      //     </div>
-      //   </div>
-      // </div>
-      // <div className={style.card}>
-      //   <div className={style.post}>
-      //     <div className={style.cover}>
-      //       <a
-      //         target="_blank"
-      //         rel="nofollow noopener noreferrer"
-      //         href={"https://github.com/robbyki"}
-      //       >
-      //         <img src={Nvim} height="200px" width="200px" alt="nvim" />
-      //       </a>
-      //     </div>
-      //     <div className={style.content}>
-      //       <span>
-      //         I have a pretty popular Neovim config over on Github if you're
-      //         interested. A lot of the process to create it has been documented{" "}
-      //         <Link to={"/neovim"}>here</Link>.
-      //       </span>
-      //     </div>
-      //   </div>
-      // </div>
-      // <div className={style.card}>
-      //   <div className={style.post}>
-      //     <div className={style.cover}>
-      //       <a
-      //         target="_blank"
-      //         rel="nofollow noopener noreferrer"
-      //         href={
-      //           "https://spacenews.com/lockheed-martin-offering-new-satellite-image-analysis-service/"
-      //         }
-      //       >
-      //         <img src={Gatr} height="150px" width="300px" alt="blog" />
-      //       </a>
-      //     </div>
-      //     <div className={style.content}>
-      //       <span>
-      //         During my time at Lockheed Martin I worked with some awesome
-      //         engineers and was the software lead for a project called G.A.T.R.
-      //       </span>
-      //     </div>
-      //   </div>
-      // </div>
+  // <div className={style.card}>
+  //   <div className={style.post}>
+  //     <div className={style.cover}>
+  //       <a
+  //         target="_blank"
+  //         rel="nofollow noopener noreferrer"
+  //         href={"https://github.com/robbyki/blog"}
+  //       >
+  //         <img src={ThisBlog} height="150px" width="225px" alt="blog" />
+  //       </a>
+  //     </div>
+  //     <div className={style.content}>
+  //       <span>Is available on my github you can fork it if you want.</span>
+  //     </div>
+  //   </div>
+  // </div>
+  // <div className={style.card}>
+  //   <div className={style.post}>
+  //     <div className={style.cover}>
+  //       <a
+  //         target="_blank"
+  //         rel="nofollow noopener noreferrer"
+  //         href={"https://github.com/robbyki"}
+  //       >
+  //         <img src={Nvim} height="200px" width="200px" alt="nvim" />
+  //       </a>
+  //     </div>
+  //     <div className={style.content}>
+  //       <span>
+  //         I have a pretty popular Neovim config over on Github if you're
+  //         interested. A lot of the process to create it has been documented{" "}
+  //         <Link to={"/neovim"}>here</Link>.
+  //       </span>
+  //     </div>
+  //   </div>
+  // </div>
+  // <div className={style.card}>
+  //   <div className={style.post}>
+  //     <div className={style.cover}>
+  //       <a
+  //         target="_blank"
+  //         rel="nofollow noopener noreferrer"
+  //         href={
+  //           "https://spacenews.com/lockheed-martin-offering-new-satellite-image-analysis-service/"
+  //         }
+  //       >
+  //         <img src={Gatr} height="150px" width="300px" alt="blog" />
+  //       </a>
+  //     </div>
+  //     <div className={style.content}>
+  //       <span>
+  //         During my time at Lockheed Martin I worked with some awesome
+  //         engineers and was the software lead for a project called G.A.T.R.
+  //       </span>
+  //     </div>
+  //   </div>
+  // </div>
 }
 export default IndexPage
